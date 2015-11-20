7c4f376e21ee597011bec3d94ef33281cb97beca
Matthew Siu <Siunami@Matthews-MacBook-Pro-3.local>
2015-11-20 00:40:28 -0800

    Adding ininitial files

---
+++ challenge.js
@@ -0,0 +1,45 @@
+/*
+for (var line = "#"; line.length<8; line += "#")
+  console.log(line);
+#
+##
+###
+####
+#####
+######
+#######
+*/
+/*for (number=1; number<101; number++){
+  if(number % 3 == 0 && number % 5 != 0){
+    console.log(number + "Fizz");
+  }else if (number % 5 == 0 && number % 3 != 0){
+    console.log(number + "Buzz");
+  }else if (number % 3 == 0 && number % 5 == 0){
+    console.log(number + "FizzBuzz");
+  }else{
+    console.log(number);
+  }
+}
+The FizzBuzz Challenge*/
+
+/*size = 8;
+board = "";
+for(var y = 0; y<size; y++){
+  for(var x = 0; x<size; x++){
+    if((x+y) % 2 == 0)
+      board += " ";
+    else if ((x+y) % 2 != 0)
+      board += "#";
+  }
+  board += "\n";
+}
+console.log(board);
+
+Chessboard pattern*/
+
+
+function greet(who) { 
+    console.log("Hello " + who);
+}
+greet("Harry"); 
+console.log("Bye");
