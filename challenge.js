114af9a6924f7ecf7e78e57b625f3144f2ad27c6
Matthew Siu <Siunami@Matthews-MacBook-Pro-3.local>
2015-11-21 22:52:43 -0800

    Fixed bug with right triangle challenge

--- challenge.js
+++ challenge.js
@@ -21,7 +21,12 @@ for (number=1; number<101; number++){
     console.log(number);
   }
 }
-/*The FizzBuzz Challenge*/
+/*The FizzBuzz Challenge
+For an interval 1-100,
+If the number is divisible by 3, print Fizz
+If the number is divisible by 5, print Buzz,
+If the number is divisible by both, print FizzBuzz
+*/
 
 size = 8;
 board = "";
@@ -51,9 +56,20 @@ console.log("Bye");
 */
 var size = function(size){
   for(size; size>0; size--){
-      for(var x = "#"; (x.length) ; x += "#"){
+      for(var x = "#"; (x.length) <= size; x += "#"){
         console.log(x);
     }
   }
 }
-size(3);
+size(4);
+/*For any size, create right triangles of that size and for each size less than that
+Ex: Size(3)
+
+Output:
+#
+##
+### (End of size three right triangle.)
+#
+## (End of size two right triangle.)
+# (End of size one right triangle.)
+*/
