4978c6b660dc9d9fe9aaf944a459d0a724799ebe
Matthew <siunami.matt@gmail.com>
2015-11-30 13:40:12 -0800

    Common Words Program Complete

--- Common_Words/CommonWord.js
+++ Common_Words/CommonWord.js
@@ -1,22 +1,13 @@
-/*var words = "He had one of those rare smiles with a quality of eternal reassurance in it, that you may come across four or five times in life. It faced, or seemed to face, the whole external world for an instant and then concentrated on you with an irresistible prejudice in your favor. It understood you just as far as you wanted to be understood, believed in you as you would like to believe in yourself.";
-var punctuationless = words.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"");
-var finalString = punctuationless.replace(/\s{2,}/g," ");
-WordList = finalString.split(" ");*/
-var words = "hi hi hi hi jk";
 var bestMode = 1;
 var currentMode = 0;
 var character;
-var WordList;
 
-function Find_Word(){
-  var punctuationless = words.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"");
-  var finalString = punctuationless.replace(/\s{2,}/g," ");
-  WordList = finalString.split(" ");
-  function FindMode(Wordlist){}
-}
+var words = "hi life my hi this is code hi";
+var punctuationless = words.replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"");
+var finalString = punctuationless.replace(/\s{2,}/g," ");
+var WordList = finalString.split(" ");
 
-function FindMode(){
-  for(var i=0; i<WordList.length; i++){
+for(var i=0; i<WordList.length; i++){
     for(var m=i; m<WordList.length; m++){
       if(WordList[i] == WordList[m]){
         currentMode += 1;
@@ -27,14 +18,5 @@ function FindMode(){
       }
     }
     currentMode = 0;
-  }
 }
-console.log(character);
-/*while(character == undefined){
-  if(character == undefined){
-    console.log("hi");
-  }else{
-    console.log(character + " occured " + bestMode + " times.");
-    break;
-  }
-}*/
+console.log(character + " occured " + bestMode + " times.");
